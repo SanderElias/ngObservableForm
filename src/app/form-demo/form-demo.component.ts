56e6c690d4c659349bba77bb5aa0acf6fd450854
Sander Elias <sanderelias+git@gmail.com>
2018-07-25 07:21:31 +0200

    updated deps

--- src/app/form-demo/form-demo.component.ts
+++ src/app/form-demo/form-demo.component.ts
@@ -17,7 +17,7 @@ export class FormDemoComponent implements OnInit {
     this.formData$.subscribe(data => {
       this.progress =
         Object.values(data).filter(i => i !== undefined).length /
-        Object.keys(data).length *100;
+        Object.keys(data).length * 100;
       console.log(this.progress, data);
     });
   }
